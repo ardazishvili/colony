e9cfb1dc4a7a15f16a99a2f9c4944ea6a440309e
ardazishvili <ardazishvili.roman@yandex.ru>
2019-12-23 02:52:11 +0400

    fix AbstractPlantRender

--- view/AbstractPlantView.cpp
+++ view/AbstractPlantView.cpp
@@ -7,6 +7,8 @@ AbstractPlantView::AbstractPlantView(Shader& shader, glm::vec3 position) :
 
 void AbstractPlantView::draw()
 {
+  _shader.use();
+  _shader.configure();
   _shader.setBool("animated", false);
   auto model = glm::mat4(1.0f);
   model = glm::translate(model, _position);
