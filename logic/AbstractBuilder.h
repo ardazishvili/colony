663e1da277c6609a9f43b3ea05c740405c8ea24e
ardazishvili <ardazishvili.roman@yandex.ru>
2020-01-11 03:35:32 +0400

    supress copying of **Builder classes

--- logic/AbstractBuilder.h
+++ logic/AbstractBuilder.h
@@ -16,9 +16,15 @@ public:
   AbstractBuilder(Game* game) : _game(game)
   {
   }
+
   virtual void create() = 0;
   virtual MenuTextures getPreviewType() = 0;
+
   ~AbstractBuilder() = default;
+  AbstractBuilder(const AbstractBuilder&) = delete;
+  AbstractBuilder(AbstractBuilder&&) = delete;
+  AbstractBuilder& operator=(const AbstractBuilder&) = delete;
+  AbstractBuilder& operator=(AbstractBuilder&&) = delete;
 
 protected:
   Game* _game;
