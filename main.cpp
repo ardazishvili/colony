9a28121a74d6966536059b7adc7ce935cb062dde
ardazishvili <ardazishvili.roman@yandex.ru>
2020-01-31 20:14:21 +0400

    add Core class with logger

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "fig/globals.h"
+
 #include "figImpl/ColonyApplication.h"
 #include "figImpl/globals.h"
 
@@ -5,11 +7,15 @@ int main(int argc, char** argv)
 {
   try {
     auto app = gApp();
-    FG_APP_WARN("warning message");
-    FG_APP_CRITICAL("critical message");
-
+    FG_APP_WARN("warning message")
+    FG_APP_CRITICAL("critical message")
     auto a = 5;
     FG_APP_ERROR("printing example {0}", a)
+
+    FG_CORE_WARN("warning message")
+    FG_CORE_CRITICAL("critical message")
+    auto b = 5;
+    FG_CORE_ERROR("printing example {0}", b)
 
     app->run();
   } catch (const std::exception& e) {
