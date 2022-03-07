48ac563fe29e4be1c72d2d068e204b01fe2d5e77
ardazishvili <ardazishvili.roman@yandex.ru>
2022-03-07 19:02:52 +0300

    Set custom handlers for keyboard keys

--- figImpl/events/ColonyKeyReleaseEvent.cpp
+++ figImpl/events/ColonyKeyReleaseEvent.cpp
@@ -4,89 +4,138 @@
 
 #include <memory>
 
+#include "EventManager.h"
 #include "figImpl/ColonyEventManager.h"
 #include "logic/structures/Barrier.h"
 #include "logic/structures/Hq.h"
 #include "logic/structures/TankFactory.h"
 #include "logic/structures/Turbine.h"
+#include "logic/units/Tank.h"
 
 ColonyKeyReleaseEvent::ColonyKeyReleaseEvent(int key, int scancode, int mods)
-    : KeyboardReleaseEvent(key, scancode, mods) {}
+    : KeyboardReleaseEvent(key, scancode, mods) {
+  _handler.setHandler(
+      GLFW_KEY_X,
+      [this](fig::EventManager* manager) { buildTankFactory(manager); },
+      "Build tank factory");
+  _handler.setHandler(
+      GLFW_KEY_Z, [this](fig::EventManager* manager) { createTank(manager); },
+      "Create tank");
+  _handler.setHandler(
+      GLFW_KEY_C, [this](fig::EventManager* manager) { buildHq(manager); },
+      "Build HQ");
+  _handler.setHandler(
+      GLFW_KEY_B, [this](fig::EventManager* manager) { buildBarrier(manager); },
+      "Build Barrier");
+  _handler.setHandler(
+      GLFW_KEY_T, [this](fig::EventManager* manager) { buildTurbine(manager); },
+      "Build Turbine");
 
-void ColonyKeyReleaseEvent::process(fig::Camera* camera,
-                                    fig::EventManager* eventManager) {
+  _handler.setHandler(
+      GLFW_KEY_LEFT_SHIFT,
+      [this](fig::EventManager* manager) { processLEFT_SHIFT(manager); },
+      "LEFT_SHIFT handler");
+  _handler.setHandler(
+      GLFW_KEY_ESCAPE,
+      [this](fig::EventManager* manager) { closeWindow(manager); },
+      "Close window");
+}
+void ColonyKeyReleaseEvent::buildTankFactory(fig::EventManager* eventManager) {
   // TODO downcast
-  auto em = dynamic_cast<ColonyEventManager*>(eventManager);
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+  std::cout << "X pressed" << std::endl;
+  if (manager->_structureToBuild == nullptr) {
+    manager->_structureToBuildStage = BuildStage::SetAngle;
+    auto tankFactory = std::make_unique<TankFactory>(
+        manager->_astar,
+        fig::EventManager::unProject(manager->_window, manager->_view,
+                                     manager->_projection));
+    manager->setStructureToBuild(tankFactory.get());
+    manager->_game.addStructure(std::move(tankFactory));
+  } else {
+    manager->_structureToBuild->commit();
+    manager->_structureToBuild = nullptr;
+  }
+}
 
-  if (_key == GLFW_KEY_LEFT_SHIFT) {
-    em->releaseKey(fig::KeyButton::LEFT_SHIFT);
+void ColonyKeyReleaseEvent::createTank(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  if (manager->_structureSelected) {
+    auto factory = dynamic_cast<TankFactory*>(manager->_structureSelected);
+    factory->createTank(manager->_game, Tank::Type::Medium, HealthLevel::High,
+                        Shell::Size::Medium);
   }
-  if (_key == GLFW_KEY_Z) {
-    if (em->_structureSelected) {
-      auto factory = dynamic_cast<TankFactory*>(em->_structureSelected);
-      factory->createTank(em->_game, Tank::Type::Medium, HealthLevel::High,
-                          Shell::Size::Medium);
-    }
+}
+
+void ColonyKeyReleaseEvent::buildHq(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  std::cout << "C pressed" << std::endl;
+  if (manager->_structureToBuild == nullptr) {
+    manager->_structureToBuildStage = BuildStage::SetAngle;
+    auto hq = std::make_unique<Hq>(
+        manager, manager->_astar,
+        fig::EventManager::unProject(manager->_window, manager->_view,
+                                     manager->_projection),
+        manager->_terrain);
+    manager->setStructureToBuild(hq.get());
+    manager->_game.addStructure(std::move(hq));
+  } else {
+    manager->_structureToBuild->commit();
+    manager->_structureToBuild = nullptr;
   }
-  if (_key == GLFW_KEY_X) {
-    std::cout << "X pressed" << std::endl;
-    if (em->_structureToBuild == nullptr) {
-      em->_structureToBuildStage = BuildStage::SetAngle;
-      auto tankFactory = std::make_unique<TankFactory>(
-          em->_astar, fig::EventManager::unProject(em->_window, em->_view,
-                                                   em->_projection));
-      em->setStructureToBuild(tankFactory.get());
-      em->_game.addStructure(std::move(tankFactory));
-    } else {
-      em->_structureToBuild->commit();
-      em->_structureToBuild = nullptr;
-    }
+}
+void ColonyKeyReleaseEvent::buildBarrier(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  std::cout << "B pressed" << std::endl;
+  if (manager->_structureToBuild == nullptr) {
+    manager->_structureToBuildStage = BuildStage::SetAngle;
+    auto barrier = std::make_unique<Barrier>(
+        fig::EventManager::unProject(manager->_window, manager->_view,
+                                     manager->_projection),
+        manager->_terrain, manager->_astar);
+    manager->_game.addShroud(barrier->shroud());
+    manager->setStructureToBuild(barrier.get());
+    manager->_game.addStructure(std::move(barrier));
+  } else {
+    manager->_structureToBuild->commit();
+    manager->_structureToBuild = nullptr;
   }
-  if (_key == GLFW_KEY_C) {
-    std::cout << "C pressed" << std::endl;
-    if (em->_structureToBuild == nullptr) {
-      em->_structureToBuildStage = BuildStage::SetAngle;
-      auto hq = std::make_unique<Hq>(
-          em, em->_astar,
-          fig::EventManager::unProject(em->_window, em->_view, em->_projection),
-          em->_terrain);
-      em->setStructureToBuild(hq.get());
-      em->_game.addStructure(std::move(hq));
-    } else {
-      em->_structureToBuild->commit();
-      em->_structureToBuild = nullptr;
-    }
+}
+void ColonyKeyReleaseEvent::buildTurbine(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  std::cout << "T pressed" << std::endl;
+  if (manager->_structureToBuild == nullptr) {
+    manager->_structureToBuildStage = BuildStage::SetAngle;
+    auto turbine = std::make_unique<Turbine>(
+        manager->_game,
+        fig::EventManager::unProject(manager->_window, manager->_view,
+                                     manager->_projection));
+    manager->setStructureToBuild(turbine.get());
+    manager->_game.addStructure(std::move(turbine));
+  } else {
+    manager->_structureToBuild->commit();
+    manager->_structureToBuild = nullptr;
   }
-  if (_key == GLFW_KEY_B) {
-    std::cout << "B pressed" << std::endl;
-    if (em->_structureToBuild == nullptr) {
-      em->_structureToBuildStage = BuildStage::SetAngle;
-      auto barrier = std::make_unique<Barrier>(
-          fig::EventManager::unProject(em->_window, em->_view, em->_projection),
-          em->_terrain, em->_astar);
-      em->_game.addShroud(barrier->shroud());
-      em->setStructureToBuild(barrier.get());
-      em->_game.addStructure(std::move(barrier));
-    } else {
-      em->_structureToBuild->commit();
-      em->_structureToBuild = nullptr;
-    }
-  }
-  if (_key == GLFW_KEY_T) {
-    std::cout << "T pressed" << std::endl;
-    if (em->_structureToBuild == nullptr) {
-      em->_structureToBuildStage = BuildStage::SetAngle;
-      auto turbine = std::make_unique<Turbine>(
-          em->_game, fig::EventManager::unProject(em->_window, em->_view,
-                                                  em->_projection));
-      em->setStructureToBuild(turbine.get());
-      em->_game.addStructure(std::move(turbine));
-    } else {
-      em->_structureToBuild->commit();
-      em->_structureToBuild = nullptr;
-    }
-  }
-  if (_key == GLFW_KEY_ESCAPE) {
-    em->_window.close();
-  }
+}
+
+void ColonyKeyReleaseEvent::processLEFT_SHIFT(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  manager->releaseKey(fig::KeyButton::LEFT_SHIFT);
+}
+
+void ColonyKeyReleaseEvent::closeWindow(fig::EventManager* eventManager) {
+  // TODO downcast
+  auto* manager = dynamic_cast<ColonyEventManager*>(eventManager);
+
+  manager->_window.close();
 }
--- figImpl/events/ColonyKeyReleaseEvent.h
+++ figImpl/events/ColonyKeyReleaseEvent.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include "EventManager.h"
 #include "fig/events/Event.h"
 
 class ColonyKeyReleaseEvent : public fig::KeyboardReleaseEvent {
  public:
   ColonyKeyReleaseEvent(int key, int scancode, int mods);
-  void process(fig::Camera* camera, fig::EventManager* eventManager);
+  void buildTankFactory(fig::EventManager* manager);
+  void createTank(fig::EventManager* manager);
+  void buildHq(fig::EventManager* manager);
+  void buildBarrier(fig::EventManager* manager);
+  void buildTurbine(fig::EventManager* manager);
+  void processLEFT_SHIFT(fig::EventManager* manager);
+  void closeWindow(fig::EventManager* manager);
 };
